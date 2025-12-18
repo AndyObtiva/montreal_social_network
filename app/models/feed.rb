c5618d84c92b26b75f3038b68663e64ce1a7ad2c
Andy Maleh <andy.am@gmail.com>
2025-12-17 20:15:28 -0500

    Implement the followed users feed (following feed)

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,10 @@
 class Feed < ApplicationRecord
   belongs_to :user
   has_many :posts
+  
+  def followed_posts
+    followed_users = user.followed
+    followed_feeds = Feed.where(user: followed_users)
+    Post.where(feed: followed_feeds)
+  end
 end
